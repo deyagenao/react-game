a494e10c816867c4df050bc7818756f96afe2966
Deya Genao <deyagenao@gmail.com>
2020-01-06 21:12:18 -0500

    Updates

---
+++ precache-manifest.c38b124411e6185ca0a3fe1359ed513f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7f87827d7da123c2c41c2e0bfa01d29",
+    "url": "/react-game/index.html"
+  },
+  {
+    "revision": "bdaac82194059fb7fec6",
+    "url": "/react-game/static/css/main.cb320bd3.chunk.css"
+  },
+  {
+    "revision": "51d9f637e680764e1c58",
+    "url": "/react-game/static/js/2.4d773306.chunk.js"
+  },
+  {
+    "revision": "bdaac82194059fb7fec6",
+    "url": "/react-game/static/js/main.74bfedda.chunk.js"
+  },
+  {
+    "revision": "efcfebfcc36ebfabd18f",
+    "url": "/react-game/static/js/runtime-main.bdeb6020.js"
+  }
+]);
